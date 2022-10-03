a42ebcd69c03c9c2251827dad5419b29e6ef124b
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 22:08:41 -0400

    Updates

---
+++ precache-manifest.e3465a89b71153bd7d142b647d06cb98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e8dc9a0917e8c3700b340f72504b54",
+    "url": "/unsplashStockPhotos/index.html"
+  },
+  {
+    "revision": "64de4df0042fb2fd2a7f",
+    "url": "/unsplashStockPhotos/static/css/main.a44d4aa5.chunk.css"
+  },
+  {
+    "revision": "833aaddd2aad21d82868",
+    "url": "/unsplashStockPhotos/static/js/2.f9fac789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unsplashStockPhotos/static/js/2.f9fac789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64de4df0042fb2fd2a7f",
+    "url": "/unsplashStockPhotos/static/js/main.9cdcec9e.chunk.js"
+  },
+  {
+    "revision": "174e23fddd9e17077359",
+    "url": "/unsplashStockPhotos/static/js/runtime-main.b2c5b41c.js"
+  }
+]);
